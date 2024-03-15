6d1d3c381138d27923fb335773472991ce0f14a5
Huy Ngo <huyntt@gmail.com>
2024-03-15 13:50:36 +0700

    update

--- k8s/install.sh
+++ k8s/install.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+
+_red() {
+    printf '\033[0;31;31m%b\033[0m' "$1"
+}
+
+_green() {
+    printf '\033[0;31;32m%b\033[0m' "$1"
+}
+
+_yellow() {
+    printf '\033[0;31;33m%b\033[0m' "$1"
+}
+
+_blue() {
+    printf '\033[0;31;36m%b\033[0m' "$1"
+}
+
 get_opsy() {
     [ -f /etc/redhat-release ] && awk '{print $0}' /etc/redhat-release && return
     [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
