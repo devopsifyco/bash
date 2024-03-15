156003653eae9cc5799e8a9d4876810ebe074f49
Huy Ngo <huyntt@gmail.com>
2024-03-15 13:46:15 +0700

    update

---
+++ k8s/README.md
@@ -0,0 +1,12 @@
+# bash
+
+
+# Usage
+## Teddysun bench
+```
+wget -qO- https://bash.opsify.dev/setup-k8s | bash
+
+curl -Lso- https://bash.opsify.dev/setup-k8s | bash
+```
+
+
--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,46 +1,30 @@
-echo "step1- install kubectl,kubeadm and kubelet 1.28.0"
+#!/usr/bin/env bash
+get_opsy() {
+    [ -f /etc/redhat-release ] && awk '{print $0}' /etc/redhat-release && return
+    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+    [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-echo "kubeadm install"
-sudo apt update -y
-sudo apt -y install vim git curl wget kubelet=1.28.0-00 kubeadm=1.28.0-00 kubectl=1.28.0-00
+# Get System information
+get_system_info() {
+    opsy=$(get_opsy)
+    arch=$(uname -m)
+}
 
-echo "memory swapoff"
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-sudo swapoff -a
-sudo modprobe overlay
-sudo modprobe br_netfilter
+print_system_info() {
+    echo " OS                 : $(_blue "$opsy")"
+    echo " Arch               : $(_blue "$arch ($lbit Bit)")"
+}
 
-echo "Containerd setup"
-sudo tee /etc/modules-load.d/containerd.conf <<EOF
-overlay
-br_netfilter
-EOF
-sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
-EOF
-sysctl --system
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update -y
-echo -ne '\n' | sudo apt-get -y install containerd
-mkdir -p /etc/containerd
-containerd config default > /etc/containerd/config.toml
-sudo systemctl restart containerd
-sudo systemctl enable containerd
-sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-sudo systemctl restart containerd
-sudo systemctl enable kubelet
-echo "image pull and cluster setup"
-sudo kubeadm config images pull --cri-socket unix:///run/containerd/containerd.sock --kubernetes-version v1.28.0
-sudo kubeadm init   --pod-network-cidr=10.244.0.0/16   --upload-certs --kubernetes-version=v1.28.0  --control-plane-endpoint=$(hostname) --ignore-preflight-errors=all  --cri-socket unix:///run/containerd/containerd.sock
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-export KUBECONFIG=/etc/kubernetes/admin.conf
-echo "Apply flannel network"
-kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
-kubectl taint node $(hostname) node-role.kubernetes.io/control-plane:NoSchedule-
+print_intro() {
+    echo "-------------------- A K8S Installation Script By DevOpsify -------------------"
+    echo " Version            : $(_green v2024-03-08)"
+    echo " Usage              : $(_red "wget -qO- https://go.opsify.dev/setup-k8s | bash")"
+}
+
+start_time=$(date +%s)
+get_system_info
+clear
+print_intro
+next
+print_system_info
